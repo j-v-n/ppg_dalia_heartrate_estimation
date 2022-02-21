4b45a52da646ca7d3b0e6ac90fc49c3c2e2d1f1a
j-v-n <jayanthvasanthnair@gmail.com>
2022-02-20 19:47:57 -0600

    Second commit. Updated requirements.txt and added other extraneous files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Estimate heartrate using PPG Sensor measurements. Dataset : https://archive.ics.uci.edu/ml/datasets/PPG-DaLiA',
+    author='Jayanth Nair',
+    license='MIT',
+)
